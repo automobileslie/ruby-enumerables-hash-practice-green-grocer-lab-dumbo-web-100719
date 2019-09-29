5aab64658f2165fbccea8d5e2c90ffb15621f304
Carlie Anglemire <carlie.anglemire@gmail.com>
2019-09-29 13:43:15 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -43,7 +43,7 @@ def apply_coupons(cart, coupons)
     
     cart["#{item} W/COUPON"][:count] += coupon[:num]
     
-    cart[item][:count] -= coupon[:num]
+    cart[:item][:count] -= coupon[:num]
 
 end
 end
